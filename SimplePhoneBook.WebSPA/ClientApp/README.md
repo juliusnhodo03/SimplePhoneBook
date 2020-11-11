2e845d584aa2c86b32628ded2a71753611145bcb
ikommaxey <ikommaxey@gmail.com>
2020-11-11 12:44:40 +0200

    renamed the WebSPA project.

--- SimplePhoneBook.WebSPA/ClientApp/README.md
+++ SimplePhoneBook.WebSPA/ClientApp/README.md
@@ -1,4 +1,4 @@
-# SimplePhoneBook.API.WebSPA
+# SimplePhoneBook.WebSPA
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.0.
 
--- SimplePhoneBook.WebSPA/Startup.cs
+++ SimplePhoneBook.WebSPA/Startup.cs
@@ -1,12 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace SimplePhoneBook.API.WebSPA
+namespace SimplePhoneBook.WebSPA
 {
     public class Startup
     {
